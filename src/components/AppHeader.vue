99385bb91a9f3b55c3b7d25d7cdcc66e8291f3c4
Nikita Bychkov <n.bychkov@noda.live>
2025-10-19 13:21:09 +0200

    feat: add empty header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  header
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+  <AppHeader />
   <v-main>
     <router-view />
   </v-main>
